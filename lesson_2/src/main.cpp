bbb439fdb0112b6d9f29362869c9d11c8085f498
Polyakov Pavel <dr.kommyhap2017@yandex.ru>
2019-09-25 14:13:06 +0700

    rid of the floating point

--- lesson_2/src/main.cpp
+++ lesson_2/src/main.cpp
@@ -23,12 +23,16 @@ void DoLine(int x0, int y0, int x1, int y1, TGA::Image & image, TGA::Color color
 		swap(x0, x1);
 		swap(y0, y1);
 	}
+	
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int derror = abs(dy) * 2;
+	int error = 0;
+
+	int y = y0;
 
 	for (int x = x0; x <= x1; ++x)
 	{
-		float t = (x - x0) / static_cast<float>(x1 - x0);
-		int y = y0 + t * (y1 - y0);
-
 		if (isSteep)
 		{
 			image.set(y, x, color);
@@ -36,6 +40,13 @@ void DoLine(int x0, int y0, int x1, int y1, TGA::Image & image, TGA::Color color
 		else
 		{
 			image.set(x, y, color);
+		}
+
+		error += derror;
+		if (error > dx)
+		{
+			y += (y1 > y0) ? 1 : -1;
+			error -= dx * 2;
 		}
 	}
 }
@@ -46,8 +57,8 @@ int main(int argc, char ** argv)
 	TGA::Image image {w, h, 3};
 
 	DoLine(0, 0, w, h, image, TGA::red);
-	DoLine(33, 22, 77, 42, image, TGA::green);
-	DoLine(w / 2, 0, w / 2 + 1, h, image, TGA::blue);
+	DoLine(w, 92, 59, 2, image, TGA::green);
+	DoLine(w / 2, 0, w / 2, h, image, TGA::blue);
 
 	if (!image.write_tga_file("test.tga"))
 	{
