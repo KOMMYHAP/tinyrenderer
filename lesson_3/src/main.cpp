c19041e3a3fa2f9a4d8987c59f91f383ae362cac
Polyakov Pavel <dr.kommyhap2017@yandex.ru>
2019-09-26 04:49:29 +0700

    lesson 3
    rendered african_head

--- lesson_3/src/main.cpp
+++ lesson_3/src/main.cpp
@@ -1,7 +1,9 @@
 #include <cmath>
 #include <utility>
+#include <iostream>
 
 #include "tgaimage.h"
+#include "model.h"
 
 void DoLine(int x0, int y0, int x1, int y1, TGA::Image & image, TGA::Color color);
 
@@ -53,6 +55,38 @@ void DoLine(int x0, int y0, int x1, int y1, TGA::Image & image, TGA::Color color
 
 int main(int argc, char ** argv)
 {
-	
+	auto model = ObjModel::LoadFromFile("../../models/african_head.obj");
+	if (!model)
+	{
+		std::cerr << "Cannot load model from file!" << std::endl;
+		return 1;
+	}
+
+	int w = 1000, h = 1000;
+	TGA::Image image {w, h, 3};
+
+	for (auto && face : model->faces)
+	{
+		for (int i = 0; i < 3; ++i)
+		{
+			auto v0 = model->vertexes[face[i]];
+			auto v1 = model->vertexes[face[(i + 1) % 3]];
+			
+			int x0 = (v0.x + 1.0f) / 2 * (w - 1);
+			int y0 = (v0.y + 1.0f) / 2 * (h - 1);
+			int x1 = (v1.x + 1.0f) / 2 * (w - 1);
+			int y1 = (v1.y + 1.0f) / 2 * (h - 1);
+
+			DoLine(x0, y0, x1, y1, image, TGA::white);
+		}
+	}
+
+	image.flip_vertically();
+	if (!image.write_tga_file("lesson_3_result.tga"))
+	{
+		std::cerr << "Cannot write result tga file!" << std::endl;
+		return 1;
+	}
+
 	return 0;
 }
