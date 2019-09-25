34436b88cd6087551a57222386169c23f465515d
Polyakov Pavel <dr.kommyhap2017@yandex.ru>
2019-09-25 13:40:34 +0700

    lesson 2
    removed holes, when dx << dy
    fixed symmetry test: line(a,b) == line(b,a)

--- lesson_2/src/main.cpp
+++ lesson_2/src/main.cpp
@@ -1,14 +1,42 @@
+#include <cmath>
+#include <utility>
+
 #include "tgaimage.h"
 
 void DoLine(int x0, int y0, int x1, int y1, TGA::Image & image, TGA::Color color);
 
 void DoLine(int x0, int y0, int x1, int y1, TGA::Image & image, TGA::Color color)
 {
-	for (float t = 0.0; t <= 1.0f; t += 0.001f)
+	using std::abs;
+	using std::swap;
+
+	bool isSteep = false;
+	if (abs(x0 - x1) < abs(y0 - y1))
 	{
-		int x = x0 + (x1 - x0) * t;
-		int y = y0 + (y1 - y0) * t;
-		image.set(x, y, color);
+		isSteep = true;
+		swap(x0, y0);
+		swap(x1, y1);
+	}
+
+	if (x0 > x1)
+	{
+		swap(x0, x1);
+		swap(y0, y1);
+	}
+
+	for (int x = x0; x <= x1; ++x)
+	{
+		float t = (x - x0) / static_cast<float>(x1 - x0);
+		int y = y0 + t * (y1 - y0);
+
+		if (isSteep)
+		{
+			image.set(y, x, color);
+		}
+		else
+		{
+			image.set(x, y, color);
+		}
 	}
 }
 
@@ -17,11 +45,9 @@ int main(int argc, char ** argv)
 	int w = 100, h = 100;
 	TGA::Image image {w, h, 3};
 
-	int step = 10;
-	for (int x = 0; x < w; x += step)
-	{
-		DoLine(x, 0, x, h, image, TGA::red);
-	}
+	DoLine(0, 0, w, h, image, TGA::red);
+	DoLine(33, 22, 77, 42, image, TGA::green);
+	DoLine(w / 2, 0, w / 2 + 1, h, image, TGA::blue);
 
 	if (!image.write_tga_file("test.tga"))
 	{
