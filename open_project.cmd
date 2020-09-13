fce26175a3aacec0e20f3a00d1604d3e9f5e6176
Pavel <16599808+KOMMYHAP@users.noreply.github.com>
2020-09-13 16:24:09 +0700

    move build directory to new file hierarchy

--- open_project.cmd
+++ open_project.cmd
@@ -1,11 +1,8 @@
-pushd .
-
 set source_path=%~dp0
-set build_path=C:/cmake-build/tinyrenderer/
+set build_path=%source_path%\\build
 
+pushd .
 mkdir "%build_path%"
 cd "%build_path%"
-
 cmake.exe -G"Visual Studio 15 2017" -A"x64" -T"host=x64" -B"%build_path%" -H"%source_path%" && cmake.exe --open "%build_path%"
-
 popd
